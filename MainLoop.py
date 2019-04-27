8fd79900217988eb2d83c1263397f1232f5810ba
MisterMahjong <48330615+MisterMahjong@users.noreply.github.com>
2019-04-27 15:12:11 -0400

    So I've finally got some directional changes, as miniscule as they may be. I had some hitbox stuff going on before but encountered an issue where they would lock into place, and the defintion I made to draw a rectangle over the image wouldn't jive with the collision function built into pygame. Most of the resources online deal with two objects colliding, and not a mouse click, so I may have to come up with an alternative solution soon. Either way, expect more pushes in the next few days.

--- MainLoop.py
+++ MainLoop.py
@@ -7,7 +7,7 @@ import random
 pygame.init()
 
 #Sets a caption for the game window
-pygame.display.set_caption("onHere Come The Clowns")
+pygame.display.set_caption("Here Come The Clowns")
 
 #Setting up my variables
 playx = 800
@@ -15,17 +15,30 @@ playy= 600
 playfield = pygame.display.set_mode((playx, playy))
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-x = (playx)
-y = (playy)
-clownx = 50
-clowny = 50
-timer = 0
+clownx = 250
+clowny = 150
+hboxx = 50
+hboxy = 50
+xdirectionswitch = 1000
+ydirectionswitch = 1000
+xdirection = True
+ydirection = True
 GameOver = False
-ticktock = 0
+
 
 #defining images to be called in the program
 Clowned = pygame.image.load("clown1.png")
 bg = pygame.image.load("spooktown.jpg")
+
+#Custom User Events
+
+xdirswitch = pygame.USEREVENT + 1
+ydirswitch = pygame.USEREVENT + 2
+
+#Custom Movement Events
+
+pygame.time.set_timer(xdirswitch, xdirectionswitch)
+pygame.time.set_timer(ydirswitch, ydirectionswitch)
 
 #Defining functions
 
@@ -33,36 +46,54 @@ def clown(x, y):
     global Clowned
     playfield.blit(Clowned,(x, y))
 
+def scootup():
+    global clowny
+    clowny = clowny + 3
+
+def scootdown():
+    global clowny
+    clowny = clowny - 3
+
 def shuffleright():
     global clownx
-    clownx = clownx + .1
+    clownx = clownx + 3
 
 def shuffleleft():
     global clownx
-    clownx = clownx - 1
+    clownx = clownx - 3
 
-def hitbox():
+def hitbox(boxwidth, boxheight):
     global clownx
-    pygame.draw.rect(playfield, WHITE, [clownx + 250, clowny + 50, 275, 650], 5)
+    pygame.draw.rect(playfield, WHITE, [clownx + hboxx, clowny + hboxy, boxwidth, boxheight], 5)
+
 
 #The start of the "game loop", grabs events and checks to ensure our parameter =! True
 
 while not GameOver:
+    pygame.mouse.set_visible(1)
     playfield.blit(bg, (0, 0))
     clown(clownx, clowny)
-    shuffleleft()
-    hitbox()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    if ydirection == True:
+        scootup()
+    elif ydirection == False:
+        scootdown()
+    if xdirection == True:
+        shuffleleft()
+    elif xdirection == False:
+        shuffleright()
+    for x in pygame.event.get():
+        if x.type == xdirswitch:
+            xdirection = not xdirection
+        if x.type == ydirswitch:
+            ydirection = not ydirection
+        if x.type == pygame.QUIT:
             GameOver = True
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            p, q = event.pos
-            if hitbox().collidepoint(p, q):
-                print('Successful Strike')
-        if event.type == pygame.MOUSEBUTTONUP:
+        if x.type == pygame.MOUSEBUTTONDOWN:
             print("HONK")
-        print(event)
+        if x.type == pygame.MOUSEBUTTONUP:
+            print("HONK")
+        print(x)
     pygame.display.update()
-    pygame.time.Clock().tick(60)
+    pygame.time.Clock().tick(30)
 pygame.quit()
 quit()
