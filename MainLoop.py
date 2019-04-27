274caaf01d7e48d609a1199ce49757fe431974d5
MisterMahjong <48330615+MisterMahjong@users.noreply.github.com>
2019-04-27 15:41:42 -0400

    Finally got a nice movement pattern down. Erratic, but stays (mostly) within the boundaries of the playfield. I let the program run for 30 minutes and nothing crashed AND the image stayed on screen, so I'm pretty happy with this right now. From here on i'll be focusing on getting the hitboxes implemented, and finally some kind of scoring system. After that, some sound would be nice, but one thing at a time.

--- MainLoop.py
+++ MainLoop.py
@@ -10,6 +10,7 @@ pygame.init()
 pygame.display.set_caption("Here Come The Clowns")
 
 #Setting up my variables
+
 playx = 800
 playy= 600
 playfield = pygame.display.set_mode((playx, playy))
@@ -17,8 +18,10 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 clownx = 250
 clowny = 150
-hboxx = 50
+hboxx = 0
 hboxy = 50
+xcounter = 3
+ycounter = 5
 xdirectionswitch = 1000
 ydirectionswitch = 1000
 xdirection = True
@@ -48,19 +51,26 @@ def clown(x, y):
 
 def scootup():
     global clowny
-    clowny = clowny + 3
+    clowny = clowny + 6
 
 def scootdown():
     global clowny
-    clowny = clowny - 3
+    clowny = clowny - 6
 
 def shuffleright():
     global clownx
-    clownx = clownx + 3
+    clownx = clownx + 6
 
 def shuffleleft():
     global clownx
-    clownx = clownx - 3
+    clownx = clownx - 6
+
+def reset():
+    global clownx, clowny, xdirection, ydirection
+    if clownx > 800 or clownx < 0:
+        xdirection = not xdirection
+    if clowny > 600 or clowny < 0:
+        ydirection = not ydirection
 
 def hitbox(boxwidth, boxheight):
     global clownx
@@ -70,9 +80,17 @@ def hitbox(boxwidth, boxheight):
 #The start of the "game loop", grabs events and checks to ensure our parameter =! True
 
 while not GameOver:
+
+    #These statements draw the background and the image on the playfield
+
     pygame.mouse.set_visible(1)
     playfield.blit(bg, (0, 0))
     clown(clownx, clowny)
+    hitbox(200, 200)
+
+    #These statements determine the x/y movement of the image
+
+    reset()
     if ydirection == True:
         scootup()
     elif ydirection == False:
@@ -81,11 +99,24 @@ while not GameOver:
         shuffleleft()
     elif xdirection == False:
         shuffleright()
+
+    #These statements pull from a list of events and execute the given code when the appropriate events are pulled.
+
     for x in pygame.event.get():
         if x.type == xdirswitch:
-            xdirection = not xdirection
+            if xcounter > 0:
+                xcounter = xcounter - 1
+            elif xcounter == 0:
+                xdirection = not xdirection
+                xcounter = 3
+
         if x.type == ydirswitch:
-            ydirection = not ydirection
+            if ycounter > 0:
+                ycounter = ycounter - 1
+            elif ycounter == 0:
+                ydirection = not ydirection
+                ycounter = 5
+
         if x.type == pygame.QUIT:
             GameOver = True
         if x.type == pygame.MOUSEBUTTONDOWN:
@@ -94,6 +125,6 @@ while not GameOver:
             print("HONK")
         print(x)
     pygame.display.update()
-    pygame.time.Clock().tick(30)
+    pygame.time.Clock().tick(300000)
 pygame.quit()
 quit()
