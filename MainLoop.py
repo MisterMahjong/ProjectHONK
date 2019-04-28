9db0fdfa466dc9c654b40098d3de3aedc7034821
MisterMahjong <48330615+MisterMahjong@users.noreply.github.com>
2019-04-28 01:15:15 -0400

    Got everything finished for the basic game. A scoreboard, a timer, and some win/lose conditions. Not bad for a days work. (I also removed the profanity, excuse me). Will be looking to add sound as the week goes on. Once that's finished I'll be looking to clean up the mess of code I got going here.

--- MainLoop.py
+++ MainLoop.py
@@ -4,9 +4,11 @@ import pygame
 import random
 
 #Initializing pygame module
+
 pygame.init()
 
 #Sets a caption for the game window
+
 pygame.display.set_caption("Here Come The Clowns")
 
 #Setting up my variables
@@ -24,45 +26,55 @@ xcounter = 3
 ycounter = 5
 xdirectionswitch = 1000
 ydirectionswitch = 1000
+timercountdownswitch = 1000
 xdirection = True
 ydirection = True
 GameOver = False
+score = 0
+gametimer = 30
+scorefont = pygame.font.SysFont("arial", 36, BLACK)
+victorymessage = scorefont.render("YOU HONKING WIN", 1, WHITE)
+failuremessage = scorefont.render("YOU HONKING LOSE", 1, WHITE)
 
 #defining images to be called in the program
+
 Clowned = pygame.image.load("clown1.png")
 bg = pygame.image.load("spooktown.jpg")
+loseclown = pygame.image.load("sadclown.png")
+winclown = pygame.image.load("happyclown.jpg")
 
 #Custom User Events
 
 xdirswitch = pygame.USEREVENT + 1
 ydirswitch = pygame.USEREVENT + 2
+timercountdown = pygame.USEREVENT + 3
 
 #Custom Movement Events
 
 pygame.time.set_timer(xdirswitch, xdirectionswitch)
 pygame.time.set_timer(ydirswitch, ydirectionswitch)
+pygame.time.set_timer(timercountdown, timercountdownswitch)
 
 #Defining functions
-
 def clown(x, y):
     global Clowned
     playfield.blit(Clowned,(x, y))
 
 def scootup():
     global clowny
-    clowny = clowny + 10
+    clowny = clowny + 8
 
 def scootdown():
     global clowny
-    clowny = clowny - 10
+    clowny = clowny - 8
 
 def shuffleright():
     global clownx
-    clownx = clownx + 10
+    clownx = clownx + 8
 
 def shuffleleft():
     global clownx
-    clownx = clownx - 10
+    clownx = clownx - 8
 
 def reset():
     global clownx, clowny, xdirection, ydirection
@@ -77,12 +89,16 @@ def reset():
 while not GameOver:
 
     #These statements draw the background and the image on the playfield
-
+    scoreboard = scorefont.render("HONKER POINTS:" + ' ' + str(score), 1, WHITE)
+    timeboard = scorefont.render("Time Remaining:" + ' ' + str(gametimer), 1, WHITE)
     pygame.mouse.set_visible(1)
     playfield.blit(bg, (0, 0))
-    clown(clownx, clowny)
     hitbox = pygame.draw.rect(playfield, WHITE, [clownx + hboxx, clowny + hboxy, 200, 200], 5)
+    clown(clownx, clowny)
+    playfield.blit(scoreboard, (50, 25))
+    playfield.blit(timeboard, (50, 60))
     pygame.display.update()
+
     #These statements determine the x/y movement of the image
 
     reset()
@@ -112,15 +128,34 @@ while not GameOver:
                 ydirection = not ydirection
                 ycounter = 5
 
+        if x.type == timercountdown:
+            gametimer = gametimer - 1
+
         if x.type == pygame.QUIT:
             GameOver = True
+
         if x.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = x.pos
             if hitbox.collidepoint(mouse_pos):
-                print("OH SHIT")
+                score = score + 1
+                print(score)
+                if score == 10:
+                    playfield.blit(winclown, (0,0))
+                    playfield.blit(victorymessage, (200, 300))
+                    pygame.display.update()
+                    pygame.time.delay(5000)
+                    pygame.quit()
+
         if x.type == pygame.MOUSEBUTTONUP:
             print("HONK")
-        print(x)
-    pygame.time.Clock().tick(30)
+
+        if gametimer < 0:
+            playfield.blit(loseclown, (0, 0))
+            playfield.blit(failuremessage, (200, 300))
+            pygame.display.update()
+            pygame.time.delay(5000)
+            pygame.quit()
+
+    pygame.time.Clock().tick(60)
 pygame.quit()
 quit()
